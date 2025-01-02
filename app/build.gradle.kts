143a8f08d9b1696a968ed28aaa58ed8592d3026e
Esther Stevens <estherstevens@Esthers-MacBook-Pro.local>
2025-01-02 19:07:16 +0000

    add splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "stevens.software.echojournal"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "stevens.software.echojournal"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.core.splashscreen)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
